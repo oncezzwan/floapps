cfaf931b15b3bcd6f6a56227ef3d5bcfb20c1744
oncezzwan <rahmafann@gmail.com>
2026-04-10 16:01:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,21 @@
-const CACHE_NAME = "flo-v3";
-
-const ASSETS = [
+const CACHE_NAME = "flo-v1";
+const urlsToCache = [
   "/",
-  "/index.html",
-  "/manifest.json"
+  "/index.html"
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const resClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, resClone);
-        });
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
